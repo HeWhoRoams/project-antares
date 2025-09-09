ddb9138e252f9ea32dfaef717c526efd4c577d59
Tim Niles <nilestimothyj@gmail.com>
2025-09-09 12:42:15 -0400

    catching up

--- scenes/starmap/starmap.gd
+++ scenes/starmap/starmap.gd
@@ -1,24 +1,81 @@
-# /scenes/starmap/starmap_camera.gd
-extends Camera2D
+# /scenes/starmap/starmap.gd
+# Main controller for the starmap view.
+extends Node2D
 
-@export var zoom_speed: float = 0.1
-@export var min_zoom: float = 0.5
-@export var max_zoom: float = 3.0
+@export var star_system_view_scene: PackedScene
+@export var ship_view_scene: PackedScene
+
+var selected_ship_view: ShipView = null
+
+func _ready() -> void:
+	# Connect to the PlayerManager's signal to know when a ship arrives.
+	PlayerManager.ship_arrived.connect(_on_ship_arrived)
+	_draw_galaxy()
+	_draw_player_ships()
 
 func _unhandled_input(event: InputEvent) -> void:
-	# --- Zooming Logic ---
-	# This checks for mouse wheel up/down events.
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			zoom -= Vector2(zoom_speed, zoom_speed)
-		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			zoom += Vector2(zoom_speed, zoom_speed)
-		# Clamp the zoom level to our min/max values.
-		zoom = zoom.clamp(Vector2(min_zoom, min_zoom), Vector2(max_zoom, max_zoom))
+	# Left-click to select
+	if event.is_action_pressed("ui_accept"):
+		var clicked_object = _get_object_at_position(get_global_mouse_position())
+		_select_ship(clicked_object if clicked_object is ShipView else null)
+	
+	# Right-click to issue move order
+	if event.is_action_pressed("ui_right_click"):
+		if selected_ship_view:
+			var clicked_object = _get_object_at_position(get_global_mouse_position())
+			# We can only issue move orders to StarSystemViews
+			if clicked_object is StarSystemView:
+				PlayerManager.set_ship_destination(
+					selected_ship_view.ship_data.id, 
+					clicked_object.star_system_data.id
+				)
 
-	# --- Panning Logic ---
-	# This checks if the middle mouse button is held down while the mouse is moving.
-	if event is InputEventMouseMotion and Input.is_action_pressed("ui_page_down"): # Middle mouse button
-		# We move the camera in the opposite direction of the mouse movement.
-		# The movement is scaled by the zoom to feel natural.
-		position -= event.relative * zoom
+func _get_object_at_position(screen_position: Vector2):
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = screen_position
+	query.collision_mask = 1
+	var result = space_state.intersect_point(query)
+	if not result.is_empty():
+		return result[0].collider.get_owner()
+	return null
+
+func _select_ship(ship_to_select: ShipView):
+	if selected_ship_view:
+		selected_ship_view.deselect()
+	selected_ship_view = ship_to_select
+	if selected_ship_view:
+		selected_ship_view.select()
+
+## Called when the PlayerManager emits the ship_arrived signal.
+func _on_ship_arrived(ship_data: ShipData) -> void:
+	var ship_view: ShipView = find_child(ship_data.id, true, false)
+	var new_system_location = GalaxyManager.star_systems.get(ship_data.current_system_id).position
+	
+	# Animate the ship moving to its new location.
+	if ship_view:
+		var tween = create_tween()
+		tween.tween_property(ship_view, "position", new_system_location, 0.5).set_trans(Tween.TRANS_SINE)
+
+func _draw_galaxy() -> void:
+	if not star_system_view_scene:
+		printerr("Starmap: StarSystemView scene is not set!")
+		return
+	for system_data in GalaxyManager.star_systems.values():
+		var new_system_view = star_system_view_scene.instantiate()
+		new_system_view.position = system_data.position
+		# This part is still future work, but we need to pass the data for right-clicks.
+		new_system_view.star_system_data = system_data 
+		add_child(new_system_view)
+
+func _draw_player_ships() -> void:
+	if not ship_view_scene:
+		printerr("Starmap: ShipView scene is not set!")
+		return
+	for ship_data in PlayerManager.owned_ships.values():
+		var current_system = GalaxyManager.star_systems.get(ship_data.current_system_id)
+		if current_system:
+			var new_ship_view = ship_view_scene.instantiate()
+			new_ship_view.set_ship_data(ship_data)
+			new_ship_view.position = current_system.position
+			add_child(new_ship_view)
--- scenes/starmap_objects/ship_view.gd
+++ scenes/starmap_objects/ship_view.gd
@@ -1,80 +1,19 @@
-# /scenes/starmap/starmap.gd
-# Main controller for the starmap view.
+# /scenes/starmap_objects/ship_view.gd
+# The visual representation of a ship on the starmap.
+class_name ShipView
 extends Node2D
 
-## Assign the StarSystemView scene in the editor.
-@export var star_system_view_scene: PackedScene
-## Assign the ShipView scene in the editor.
-@export var ship_view_scene: PackedScene
+var ship_data: ShipData
+var is_selected: bool = false
 
-# A variable to hold a reference to the currently selected ship.
-var selected_ship_view: ShipView = null
+func set_ship_data(new_ship_data: ShipData) -> void:
+	ship_data = new_ship_data
+	name = ship_data.id
 
-func _ready() -> void:
-	_draw_galaxy()
-	_draw_player_ships()
+func select() -> void:
+	is_selected = true
+	modulate = Color.GREEN
 
-func _unhandled_input(event: InputEvent) -> void:
-	# Check for a left mouse click.
-	if event.is_action_pressed("ui_accept"): # "ui_accept" is the default for left-click.
-		# Figure out what we clicked on.
-		var clicked_object = _get_object_at_position(get_global_mouse_position())
-		
-		# If we clicked on a ship, select it.
-		if clicked_object is ShipView:
-			_select_ship(clicked_object)
-		# If we clicked on empty space, deselect whatever we had selected.
-		else:
-			_select_ship(null)
-
-## Uses Godot's physics engine to find the topmost 2D object at a screen position.
-func _get_object_at_position(screen_position: Vector2):
-	var space_state = get_world_2d().direct_space_state
-	var query = PhysicsPointQueryParameters2D.new()
-	query.position = screen_position
-	# We only want to detect objects on physics layer 1.
-	query.collision_mask = 1
-	var result = space_state.intersect_point(query)
-	
-	if not result.is_empty():
-		# The result gives us the collider, so we get its owner (the ShipView).
-		return result[0].collider.get_owner()
-	
-	return null
-
-## Manages the selection state of ships.
-func _select_ship(ship_to_select: ShipView):
-	# If we already have a ship selected, deselect it first.
-	if selected_ship_view:
-		selected_ship_view.deselect()
-	
-	# Set the new ship as the selected one.
-	selected_ship_view = ship_to_select
-	
-	# If the new selection is actually a ship (and not null), select it.
-	if selected_ship_view:
-		selected_ship_view.select()
-
-func _draw_galaxy() -> void:
-	if not star_system_view_scene:
-		printerr("Starmap: StarSystemView scene is not set!")
-		return
-		
-	for system_data in GalaxyManager.star_systems.values():
-		var new_system_view = star_system_view_scene.instantiate()
-		new_system_view.position = system_data.position
-		# (Future) new_system_view.set_system_data(system_data)
-		add_child(new_system_view)
-
-func _draw_player_ships() -> void:
-	if not ship_view_scene:
-		printerr("Starmap: ShipView scene is not set!")
-		return
-		
-	for ship_data in PlayerManager.owned_ships.values():
-		var current_system = GalaxyManager.star_systems.get(ship_data.current_system_id)
-		if current_system:
-			var new_ship_view = ship_view_scene.instantiate()
-			new_ship_view.set_ship_data(ship_data)
-			new_ship_view.position = current_system.position
-			add_child(new_ship_view)
+func deselect() -> void:
+	is_selected = false
+	modulate = Color.WHITE
