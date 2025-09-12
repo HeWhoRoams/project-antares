0f445ebd4a790f227869093426537ffc070fc05d
Tim Niles <nilestimothyj@gmail.com>
2025-09-12 14:46:47 -0400

    tech infra updates

--- scripts/managers/DataManager.gd
+++ scripts/managers/DataManager.gd
@@ -1,6 +1,5 @@
 # /scripts/managers/DataManager.gd
 # A global singleton (Autoload) responsible for loading all game data from files.
-
 extends Node
 
 ## A dictionary to hold all loaded Technology resources, keyed by their unique 'id'.
@@ -10,10 +9,14 @@ var technologies: Dictionary = {}
 ## (Future) A dictionary for all Faction resources.
 # var factions: Dictionary = {}
 
+## NEW: A variable to hold our structured tech tree data from the JSON file.
+var tech_tree_data: Dictionary = {}
+
 
 func _ready() -> void:
 	print("DataManager: Loading all game data...")
 	_load_resources_from_directory("res://gamedata/technologies/", technologies)
+	_load_tech_tree_from_json("res://gamedata/technologies/tech_tree.json")
 	print("DataManager: All game data loaded.")
 
 
@@ -37,3 +40,22 @@ func _load_resources_from_directory(path: String, target_dictionary: Dictionary)
 			file_name = dir.get_next()
 	else:
 		printerr("DataManager: Could not open directory at path: %s" % path)
+
+## NEW: Function to load and parse the tech tree from our JSON file.
+func _load_tech_tree_from_json(path: String) -> void:
+	if not FileAccess.file_exists(path):
+		printerr("DataManager: Tech tree file not found at path: %s" % path)
+		return
+
+	var file = FileAccess.open(path, FileAccess.READ)
+	var content = file.get_as_text()
+	file.close()
+
+	var json = JSON.new()
+	var error = json.parse(content)
+	if error != OK:
+		printerr("DataManager: Failed to parse tech_tree.json. Error: %s" % json.get_error_message())
+		return
+
+	tech_tree_data = json.get_data()
+	print("  -> Loaded tech tree data from JSON.")
