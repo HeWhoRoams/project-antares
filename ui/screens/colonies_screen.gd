b981e239a85ea63a05feb24a344e8075f4b8b6e7
Tim Niles <nilestimothyj@gmail.com>
2025-09-15 14:41:17 -0400

    colony fix

--- ui/screens/colonies_screen.gd
+++ ui/screens/colonies_screen.gd
@@ -1,28 +1,24 @@
 extends Control
 
-# References to UI sections
-@onready var colony_name_label: Label = %ColonyNameLabel
-@onready var population_label: Label = %PopulationLabel
-@onready var system_planet_list: VBoxContainer = %SystemPlanetList
-@onready var resource_display: GridContainer = %ResourceDisplay
-@onready var pop_allocation_display: GridContainer = %PopulationAllocation
+@onready var colony_list_tree: Tree = %ColonyList
+@onready var planet_details_label: Label = %PlanetDetailsLabel
+@onready var population_output_label: Label = %PopulationOutputLabel
+@onready var mini_starmap_label: Label = %MiniStarmapLabel
+@onready var global_variables_label: Label = %GlobalVariablesLabel
 @onready var construction_label: Label = %ConstructionLabel
 @onready var construction_progress: ProgressBar = %ConstructionProgress
 @onready var surface_background: TextureRect = %Background
 @onready var buildings_container: Control = %BuildingsContainer
+@onready var pop_allocation_display: GridContainer = %PopulationAllocation
 
-# The PlanetData resource for the currently viewed colony
 var _current_planet: PlanetData
 
-# Preload textures for backgrounds
 const SURFACE_BACKGROUNDS = {
 	PlanetData.PlanetType.TERRAN: preload("res://assets/images/planet_surfaces/terran_surface.png"),
 	PlanetData.PlanetType.DESERT: preload("res://assets/images/planet_surfaces/desert_surface.png"),
-	# Add other surface backgrounds here
 }
 
 func _ready() -> void:
-	# Get the planet data that was passed from the previous scene
 	if SceneManager.next_scene_context is PlanetData:
 		_current_planet = SceneManager.next_scene_context
 		_update_all_displays()
@@ -35,61 +31,60 @@ func _update_all_displays() -> void:
 	if not is_instance_valid(_current_planet):
 		return
 	
-	# 1. Update Top Bar
-	colony_name_label.text = "Colony of %s" % _current_planet.name # Assuming planet has a name property
+	colony_name_label.text = "Colony of %s" % _current_planet.display_name
 	population_label.text = "Pop %d / %d" % [_current_planet.current_population, _current_planet.max_population]
 	
-	# 2. Update System Planet List (Placeholder)
-	for child in system_planet_list.get_children():
-		child.queue_free()
 	var temp_label = Label.new()
 	temp_label.text = "List of planets in system"
 	system_planet_list.add_child(temp_label)
 	
-	# 3. Update Resource Display (Placeholder)
-	for child in resource_display.get_children():
-		child.queue_free()
 	temp_label = Label.new()
 	temp_label.text = "Resource icons and numbers"
 	resource_display.add_child(temp_label)
 	
-	# 4. Update Population Allocation Display
 	_update_population_display()
 		
-	# 5. Update Construction Display (Placeholder)
 	construction_label.text = "Building: Hydroponics Farm"
 	construction_progress.value = 30
 	
-	# 6. Update Planet Surface
 	surface_background.texture = SURFACE_BACKGROUNDS.get(_current_planet.planet_type)
-	for child in buildings_container.get_children():
-		child.queue_free()
+	
 	temp_label = Label.new()
 	temp_label.text = "Constructed building sprites go here"
 	temp_label.position = Vector2(100, 100)
 	buildings_container.add_child(temp_label)
 
 func _update_population_display() -> void:
-	pop_allocation_display.get_children().map(func(c): c.queue_free())
+	for child in pop_allocation_display.get_children():
+		child.queue_free()
 	
-	var pop_icon = preload("res://assets/icons/population.png")
-	
-	var farmer_label = Label.new(); farmer_label.text = "Farmers:"
+	var pop_icon = ResourceLoader.load("res://assets/icons/population.png")
+	if not pop_icon:
+		printerr("ColoniesScreen: Could not load population icon at 'res://assets/icons/population.png'.")
+		return
+
+	var farmer_label = Label.new()
+	farmer_label.text = "Farmers:"
 	pop_allocation_display.add_child(farmer_label)
 	for i in _current_planet.farmers:
-		var icon = TextureRect.new(); icon.texture = pop_icon
+		var icon = TextureRect.new()
+		icon.texture = pop_icon
 		pop_allocation_display.add_child(icon)
 	
-	var worker_label = Label.new(); worker_label.text = "Workers:"
+	var worker_label = Label.new()
+	worker_label.text = "Workers:"
 	pop_allocation_display.add_child(worker_label)
 	for i in _current_planet.workers:
-		var icon = TextureRect.new(); icon.texture = pop_icon
+		var icon = TextureRect.new()
+		icon.texture = pop_icon
 		pop_allocation_display.add_child(icon)
 		
-	var scientist_label = Label.new(); scientist_label.text = "Scientists:"
+	var scientist_label = Label.new()
+	scientist_label.text = "Scientists:"
 	pop_allocation_display.add_child(scientist_label)
 	for i in _current_planet.scientists:
-		var icon = TextureRect.new(); icon.texture = pop_icon
+		var icon = TextureRect.new()
+		icon.texture = pop_icon
 		pop_allocation_display.add_child(icon)
 
 func _on_return_button_pressed() -> void:
