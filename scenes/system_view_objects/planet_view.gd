ba90e0de4f3050b7d6a5517bf3932b2473a9a58b
Tim Niles <nilestimothyj@gmail.com>
2025-09-21 23:26:39 -0400

    help

--- scenes/system_view_objects/planet_view.gd
+++ scenes/system_view_objects/planet_view.gd
@@ -1,56 +1,92 @@
-# /gamedata/celestial_bodies/planet_data.gd
-class_name PlanetData
-extends CelestialBodyData
+# /scenes/system_view_objects/planet_view.gd
+extends Control
 
-enum PlanetType { OCEAN, TERRAN, DESERT, ICE, BARREN }
-enum MineralRichness { VERY_LOW, LOW, NORMAL, HIGH, VERY_HIGH }
-enum Gravity { LOW, NORMAL, HIGH }
+@onready var sprite: Sprite2D = %Sprite2D
+@onready var label: Label = %Label
+@onready var hover_frame: Panel = %HoverFrame
 
-@export var planet_type: PlanetType
+var _body_data: CelestialBodyData
+var _is_mouse_over_sprite: bool = false
 
-@export_group("Colony State")
-## The ID of the empire that owns this planet. Empty if uncolonized.
-@export var owner_id: StringName = &""
-## The current number of population units.
-@export var current_population: int = 0
-## The number of population units assigned to farming.
-@export var farmers: int = 0
-## The number of population units assigned to industry.
-@export var workers: int = 0
-## The number of population units assigned to research.
-@export var scientists: int = 0
+const PLANET_TEXTURES = {
+	PlanetData.PlanetType.OCEAN: preload("res://assets/images/planets/ocean.png"),
+	PlanetData.PlanetType.TERRAN: preload("res://assets/images/planets/terran.png"),
+	PlanetData.PlanetType.DESERT: preload("res://assets/images/planets/desert.png"),
+	PlanetData.PlanetType.ICE: preload("res://assets/images/planets/ice.png"),
+	PlanetData.PlanetType.BARREN: preload("res://assets/images/planets/barren.png")
+}
 
-@export_group("Planet Attributes")
-@export var max_population: int = 12
-@export var mineral_richness: MineralRichness = MineralRichness.NORMAL
-@export var gravity: Gravity = Gravity.NORMAL
-@export var moons: int = 0
+const BODY_TEXTURES = {
+	CelestialBodyData.BodyType.GAS_GIANT: preload("res://assets/images/planets/gas_giant.png"),
+	CelestialBodyData.BodyType.ASTEROID_BELT: preload("res://assets/images/planets/asteroid_belt.png")
+}
 
-@export_group("Construction")
-## An array of BuildableItem IDs.
-@export var construction_queue: Array[StringName] = []
-@export var current_build_progress: float = 0.0
+const PLANET_SIZES = {
+	PlanetData.PlanetType.OCEAN: Vector2(80, 80),
+	PlanetData.PlanetType.TERRAN: Vector2(80, 80),
+	PlanetData.PlanetType.DESERT: Vector2(80, 80),
+	PlanetData.PlanetType.ICE: Vector2(80, 80),
+	PlanetData.PlanetType.BARREN: Vector2(80, 80)
+}
 
-@export_group("Special Features")
-@export var has_natives: bool = false
-@export var has_artifacts: bool = false
-@export var has_crashed_ship: bool = false
-@export var is_abandoned: bool = false
+const GENERAL_BODY_SIZES = {
+	CelestialBodyData.BodyType.GAS_GIANT: Vector2(150, 150),
+	CelestialBodyData.BodyType.ASTEROID_BELT: Vector2(120, 120)
+}
 
-@export_group("Fauna")
-@export var has_native_animals: bool = false
-@export var has_thriving_fauna: bool = false
-@export var has_hostile_fauna: bool = false
+const ROMAN_NUMERALS = ["I", "II", "III", "IV", "V", "VI", "VII"]
 
-func _init():
-	body_type = BodyType.PLANET
-	size = BodySize.M
+func _ready() -> void:
+	mouse_exited.connect(_on_mouse_exited)
 
-func get_mineral_modifier() -> float:
-	match mineral_richness:
-		MineralRichness.VERY_LOW: return -1.5
-		MineralRichness.LOW: return -1.0
-		MineralRichness.NORMAL: return 0.0
-		MineralRichness.HIGH: return 1.0
-		MineralRichness.VERY_HIGH: return 1.5
-	return 0.0
+func set_body_data(body_data: CelestialBodyData, system_name: String) -> void:
+	_body_data = body_data
+	var body_type_string: String
+	var target_size := Vector2(100, 100)
+
+	if body_data is PlanetData:
+		var planet_data: PlanetData = body_data
+		body_type_string = PlanetData.PlanetType.keys()[planet_data.planet_type].capitalize()
+		sprite.texture = PLANET_TEXTURES.get(planet_data.planet_type)
+		target_size = PLANET_SIZES.get(planet_data.planet_type, Vector2(80, 80))
+	else:
+		body_type_string = CelestialBodyData.BodyType.keys()[body_data.body_type].capitalize().replace("_", " ")
+		sprite.texture = BODY_TEXTURES.get(body_data.body_type)
+		target_size = GENERAL_BODY_SIZES.get(body_data.body_type, Vector2(100, 100))
+		
+	var roman_numeral = ""
+	if body_data.orbital_slot < ROMAN_NUMERALS.size():
+		roman_numeral = ROMAN_NUMERALS[body_data.orbital_slot]
+	
+	label.text = "%s %s\n%s" % [system_name, roman_numeral, body_type_string]
+	
+	if sprite.texture:
+		var tex_size = sprite.texture.get_size()
+		if tex_size.x > 0 and tex_size.y > 0:
+			var scale_x = target_size.x / tex_size.x
+			var scale_y = target_size.y / tex_size.y
+			sprite.scale = Vector2(min(scale_x, scale_y), min(scale_x, scale_y))
+
+func _gui_input(event: InputEvent) -> void:
+	# Check if the mouse is inside the sprite's rectangle, relative to the sprite's own position
+	var sprite_rect = sprite.get_rect()
+	var is_over = sprite_rect.has_point(event.position - sprite.position)
+	
+	if event is InputEventMouseMotion:
+		if is_over and not _is_mouse_over_sprite:
+			_is_mouse_over_sprite = true
+			hover_frame.show()
+			AudioManager.play_sfx("hover")
+		elif not is_over and _is_mouse_over_sprite:
+			_is_mouse_over_sprite = false
+			hover_frame.hide()
+
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		if is_over and _body_data is PlanetData and _body_data.owner_id == PlayerManager.player_empire.id:
+			AudioManager.play_sfx("confirm")
+			SceneManager.change_scene("res://ui/screens/colonies_screen.tscn", _body_data)
+
+func _on_mouse_exited() -> void:
+	# This ensures the hover frame disappears if the mouse leaves the entire component.
+	_is_mouse_over_sprite = false
+	hover_frame.hide()
